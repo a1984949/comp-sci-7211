d8fec05e5bd090ad0dbfae79a32b20011ee93205
a1984949 <a1984949@adelaide.edu.au>
2025-09-17 16:12:43 +0930

    question11.sh added

--- question11.sh
+++ question11.sh
@@ -3,7 +3,7 @@ git checkout -b branch2
 touch file4
 git add file4
 git commit -m "added git add file4"
-cho "changes added" >> file4
+echo "changes added" >> file4
 git add file4
 git stash
 git checkout main
