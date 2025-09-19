91090d3dbad6635a9f4646f420920b537be5f28c
a1984949 <a1984949@adelaide.edu.au>
2025-09-19 15:24:30 +0930

    question18.sh

---
+++ question18.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd git-practice-04
+git fetch --all --prune
+git checkout -b ready1 origin/ready1
+git checkout -b ready2 origin/ready2
+git checkout -b ready3 origin/ready3
+git checkout -b update1 origin/update1
+git checkout -b update2 origin/update2
+git checkout main
+git fetch --all --prune && git checkout main && git pull --ff-only origin main && for b in $(git for-each-ref --format='%(refname:short)' refs/remotes/origin/ready*); do git merge --no-ff --no-edit "$b" || break; done
+git add .
+git commit -m "ready branches changes"
+git branch -d ready1
+git branch -d ready2
+git branch -d ready3
+git add .
+git commit -m "commit deleted branches changes"
